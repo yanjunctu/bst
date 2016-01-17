a17d4021eed5efa0f3ed55ad377516b60e3d357e
jhv384 <jhv384@motorolasolutions.com>
2016-01-17 15:49:50 +0800

    add two new file, index.js and feedbackSummary.jade

---
+++ www/public/javascripts/index.js
@@ -0,0 +1,33 @@
+var main = function()
+{
+  var $subBtn = $("#submit_btn");
+   
+  $subBtn.click( function () {
+      
+      var feedback_rateOfGit = $("#feedback_form input[type='radio']:checked").val();
+      var feedback_advise = $("#comment").val()
+      var feedback_coreid = $("#Feedback_coreId").val()
+     
+      var newFeedBack = {"rateOfGit":feedback_rateOfGit, "selfAdvise":feedback_advise, "coreid":feedback_coreid};
+
+      try
+      {
+          $.post("/feedback/gitnew", newFeedBack, function (result) {
+          
+        })
+      }
+      catch(err)
+      {
+        alert(err.message)
+      }
+      
+      $("#comment").val("")
+      $("#feedback_form input[type='radio']").attr("checked",false)
+      $("#Feedback_coreId").val("")
+      $("#feedback_status").text("Thanks for your feedback")
+      
+      return false;
+  });  
+};
+
+$(document).ready(main);
