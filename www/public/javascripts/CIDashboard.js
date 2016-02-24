4ee375f2d4854bac706735333358032bcc4f7488
jhv384 <jhv384@motorolasolutions.com>
2016-02-24 10:43:06 +0800

    add other four plot chart, wait rurong to fill the real data

--- www/public/javascripts/CIDashboard.js
+++ www/public/javascripts/CIDashboard.js
@@ -1,11 +1,35 @@
 var main = function(){
-  TESTER = document.getElementById('tester');
+  
+  // Can't use jquery selection format $("#onbuild"), otherwise plot will not work
+  // so far not find the root cause
+  var plotHandler = document.getElementById('onbuild');
 
-  Plotly.plot( TESTER, [{
+  Plotly.plot( plotHandler, [{
       x: [1, 2, 3, 4, 5],
       y: [1, 2, 4, 8, 16] }], { 
       margin: { t: 0 } } );
 
+  plotHandler = document.getElementById('ontest');
+
+  Plotly.plot( plotHandler, [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }], { 
+      margin: { t: 0 } } );
+  
+  plotHandler = document.getElementById('offbuild');
+
+  Plotly.plot( plotHandler, [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }], { 
+      margin: { t: 0 } } );
+
+  plotHandler = document.getElementById('offtest');
+
+  Plotly.plot( plotHandler, [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }], { 
+      margin: { t: 0 } } );
+  
   /* Current Plotly.js version */
   console.log( Plotly.BUILD );
   
