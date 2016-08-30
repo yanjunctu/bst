c636471e4526a21e231b985c8aa076e3388f7659
Barry Su-PNW867 <baocheng.su@motorolasolutions.com>
2016-08-30 16:08:11 +0800

    modify bat

--- booster_win/start.bat
+++ booster_win/start.bat
@@ -1,1 +1,6 @@
-set DEBUG=myapp:* & npm start
+@echo Start Booster win server...
+@echo command is DEBUG=myapp:* & npm start
+@set DEBUG=myapp:* & npm start
+@echo [press enter to exit]
+@pause>nul
+exit
