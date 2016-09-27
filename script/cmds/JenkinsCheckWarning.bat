12c038af6c434c48c331cbf67f13b33a16bca860
yanjun-jhv384 <jhv384@motorolasolutions.com>
2016-09-27 11:20:07 +0800

    fix some issue

--- script/cmds/JenkinsCheckWarning.bat
+++ script/cmds/JenkinsCheckWarning.bat
@@ -6,6 +6,6 @@ set PATH=%~d0\cm\runtime\python27;%PATH%
 rem %1:submitter name  %2: submitter email  %3: baseline %4: releaseTag %5: audit
 rem python %~d0\ltd\tools\booster\warning\findNewWarnings.py -m CI -d %~d0 -n %1 -e %2 -b %3 -t %4 -a %5
 
-rem %1:submitter name  %2: submitter email  %3: baseline %4: releaseTag %5:project
-python %~d0\ltd\tools\booster\warning\findNewWarnings.py -m CI -d %~d0 -n %1 -e %2 -b %3 -t %4 -a N -prj %5
+rem %1:submitter name  %2: submitter email  %3: baseline %4: releaseTag
+python %~d0\ltd\tools\booster\warning\findNewWarnings.py -m CI -d %~d0 -n %1 -e %2 -b %3 -t %4 -a N
 ::pause&exit
